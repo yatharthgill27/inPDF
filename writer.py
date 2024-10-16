69540d98ffc8b3314639080cc294f0787501e383
Yatharth <yatharthgill@gmail.com>
2024-10-16 19:00:54 +0530

    V2

--- writer.py
+++ writer.py
@@ -1,7 +1,50 @@
+from PyPDF2 import PdfWriter, PdfReader
+import io
+from reportlab.pdfgen.canvas import Canvas
 import json
+
+class GenerateFromTemplate:
+    def __init__(self,template):
+        self.template_pdf = PdfReader(open(template, "rb"))
+        self.template_page= self.template_pdf.pages[0]
+
+        self.packet = io.BytesIO()
+        self.c = Canvas(self.packet,pagesize=(self.template_page.mediabox.width,self.template_page.mediabox.height))
+
+    
+    def addText(self,text,point):
+        self.c.drawString(point[0],point[1],text)
+
+    def merge(self):
+        self.c.save()
+        self.packet.seek(0)
+        result_pdf = PdfReader(self.packet)
+        result = result_pdf.pages[0]
+
+        self.output = PdfWriter()
+
+        self.template_page.merge_page(result)
+        self.output.add_page(self.template_page)
+    
+    def generate(self,dest):
+        outputStream = open(dest,"wb")
+        self.output.write(outputStream)
+        outputStream.close()
 
 
 with open("test.json") as f:
-    data = json.load(f)
+    input_data = json.load(f)
 
 
+with open("overlay.json") as f:
+    overlay_data = json.load(f)
+
+input_arr =  list(input_data.values())
+overlay_arr = list(overlay_data.values())
+
+gen = GenerateFromTemplate("test.pdf")
+
+gen.addText("hello",[200,400])
+
+gen.merge()
+gen.generate("Output.pdf")
